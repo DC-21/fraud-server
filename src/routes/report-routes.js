5ead2fd6360835a3860e022b2a08b7aa0be72b1c
DC-21 <cholahkuboko@gmail.com>
2023-10-31 13:23:29 +0200

    first commit

--- src/routes/report-routes.js
+++ src/routes/report-routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/report-controller');
 const upload = require('../config/multer');
 
-router.post('/report', upload.single('image'), userController.createReport);
+router.post('/reports', upload.single('image'), userController.createReport);
 router.get('/reports', userController.getReport);
 router.get('/reports/:id', userController.getReportById);
-router.put('/reports/:id', userController.updateReportState);
+router.put('/reports/:reportId', userController.updateReportState);
 
 module.exports = router;
